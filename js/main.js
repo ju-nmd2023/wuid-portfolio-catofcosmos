4c8b69fe076aae328752599190256c8560e2492d
Caroline K <klca23yg@student.ju.se>
2023-11-24 11:46:09 +0100

    updated page with animation
    I animated the submit button to blink three times and added a message to say thanks when submitted.
    
    My thought is to make the button blink at the same time as you get the thanks message, but haven't figured out how to merge it yet.

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+    
+
+
+const thanks = document.querySelector("#thanks");
+
+thanks.addEventListener("click", function() {
+  thanks.value = "Thanks!";
+  thanks.disabled = true;
+
+ /*blinking submit*/ 
+  let submitButton = document.querySelector(".submit");
+
+  submitButton.classList.add("blinking");
+
+  submitButton.classList.remove("blinking");
+  
+});
